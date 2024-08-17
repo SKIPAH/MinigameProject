9e241c15184605c3886200a30e1c7e31bc19974e
SKIPAH <juzzii.mehta@gmail.com>
2024-08-17 22:09:44 +0300

    moving platforms small stuff

--- Assets/Scripts/Mechanics etc/JumpPad.cs
+++ Assets/Scripts/Mechanics etc/JumpPad.cs
@@ -4,6 +4,11 @@ public class JumpPad : MonoBehaviour
 {
     [SerializeField] private float bouncePower = 50f;
 
+    [SerializeField] private bool isMovingJumpPad;
+    private float changeDirectionTimer = 2.0f;
+    private float currentXposition;
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<PlayerMonkey>())
@@ -12,4 +17,21 @@ public class JumpPad : MonoBehaviour
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bouncePower, ForceMode2D.Impulse);
         }
     }
+
+    private void Start()
+    {
+        if (isMovingJumpPad)
+        {
+            MoveJumpPad();
+            currentXposition = gameObject.transform.position.x;
+            
+        }
+    }
+
+
+    private void MoveJumpPad()
+    {
+        LeanTween.moveX(gameObject, currentXposition, changeDirectionTimer).setEaseLinear().setLoopPingPong();
+    }
+
 }
