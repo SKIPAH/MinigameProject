9b2e282bb2fcf20c37dfe28634e0b750a7fb76f0
SKIPAH <juzzii.mehta@gmail.com>
2025-01-30 22:20:08 +0200

    more sprites, mainmenu scemne

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,51 @@
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playGameButton;
+    [SerializeField] private Button instructionsButton;
+    [SerializeField] private Button quitGameButton;
+    [SerializeField] private Button closeInstructionsButton;
+
+    [SerializeField] private GameObject instructionsUI;
+
+
+    private void Awake()
+    {
+        playGameButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("MiniGame1");
+        });
+
+
+        instructionsButton.onClick.AddListener(() =>
+        {
+            ShowInstructionsUI();
+        });
+
+        quitGameButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+
+        closeInstructionsButton.onClick.AddListener(() =>
+        {
+            HideInstructionsUI();
+        });
+
+        HideInstructionsUI();
+    }
+
+
+    private void ShowInstructionsUI()
+    {
+        instructionsUI.SetActive(true);
+    }
+
+    private void HideInstructionsUI()
+    {
+        instructionsUI.SetActive(false);
+    }
+}
